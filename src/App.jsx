f36232eb8eaea1b48dbd19b4d4c9a8616372061a
NITISH RAJBHAR <96065088+nitishghht@users.noreply.github.com>
2024-07-25 20:33:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HeroBanner from './Components/HeroBanner'
+import Navbar from './Components/Navbar'
+import Dishes from './Components/Dishes'
+import About from './Components/About'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className='overflow-y-hidden text-netural-200 antialiased'>
+    <HeroBanner />
+    <Navbar />
+    <Dishes />
+    <About />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+import { LINKS } from '../constants';
+import { FaBars, FaTimes } from 'react-icons/fa';  // Importing FaBars and FaTimes from react-icons
+
+const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    }
+
+    const handleScroll = (event, targetId) => {
+        event.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            const offsetTop = targetElement.offsetTop - 80;
+            window.scrollTo({
+                top: offsetTop,
+                behavior: "smooth"
+            });
+        }
+        setIsMobileMenuOpen(false);
+    }
+
+    return (
+        <nav className='fixed top-3 z-50 w-full flex flex-col items-center'>
+            <div className="flex w-11/12 max-w-[50rem] items-center justify-between overflow-y-hidden p-4 backdrop-blur-lg rounded-full shadow-lg">
+                <img src={logo} alt="logo" width={80} height={22} />
+                <div className="hidden space-x-6 lg:flex">
+                    {LINKS.map((link, index) => (
+                        <a
+                            key={index}
+                            href={`#${link.targetId}`}
+                            className={`text-sm ${index !== 0 ? "border-l-2 border-neutral-300/20 pl-2" : ""} hover:opacity-50`}
+                            onClick={(e) => handleScroll(e, link.targetId)}
+                        >
+                            {link.text}
+                        </a>
+                    ))}
+                </div>
+                <div className='lg:hidden'>
+                    <button onClick={toggleMobileMenu}>
+                        {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+                    </button>
+                </div>
+            </div>
+            {isMobileMenuOpen && (
+                <div className='w-full backdrop-blur-lg lg:hidden'>
+                    {LINKS.map((link, index) => (
+                        <a
+                            key={index}
+                            href={`#${link.targetId}`}
+                            className='block p-4 uppercase tracking-tighter'
+                            onClick={(e) => handleScroll(e, link.targetId)}
+                        >
+                            {link.text}
+                        </a>
+                    ))}
+                </div>
+            )}
+        </nav>
+    );
+}
+
+export default Navbar;
